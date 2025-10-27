c3eb93a64463c792839a78c7dc188d98d81e4b3b
Michela <micheladellagatta1@gmail.com>
2025-10-27 12:11:29 +0100

    Add polaroids with content given by API

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,32 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
     
     result.forEach(polaroid => {
       polaroid_array.push(polaroid);
+
+      if(polaroid_array.length === 6) {
+          polaroid_container.innerHTML = "";
+          polaroid_array.forEach((polaroid) => {
+            const elem = document.createElement("div");
+            elem.className = "polaroid";
+            elem.innerHTML = `
+              <div class="pin">
+                <img src="./assets/img/pin.svg" alt="pin">
+              </div>
+              <div class="img-container">
+                <img src="${polaroid.url}" class="" alt="">
+              </div>
+              <div class="polaroid-body">
+                <p class="polaroid-date">${polaroid.date}</p>
+                <p class="polaroid-description">
+                  ${polaroid.title}
+                </p>
+              </div>
+            `;
+            polaroid_container.appendChild(elem);
+          });
+      }
     })
 
-    console.log(polaroid_array)
+    
   
   })
   .catch((error) => console.error(error));
