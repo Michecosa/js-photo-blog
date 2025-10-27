1447dbaf938cb2dbc101401a251371f164b0bbb9
Michela <micheladellagatta1@gmail.com>
2025-10-27 12:00:26 +0100

    Fix response conversion and push objects in array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
+const polaroid_container = document.getElementById('polaroid-container')
+const polaroid_array = []
+
 fetch("https://lanciweb.github.io/demo/api/pictures/")
-  .then((response) => response.text())
-  .then((result) => console.log(result))
+  .then((response) => response.json())
+  .then((result) => {
+    
+    result.forEach(polaroid => {
+      polaroid_array.push(polaroid);
+    })
+
+    console.log(polaroid_array)
+  
+  })
   .catch((error) => console.error(error));
